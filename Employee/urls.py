588b623236057b864dcdd3d19f44ac866987b706
praseesh <praseeshprasee@gmail.com>
2024-02-29 10:36:49 +0530

    Added update, display,delete functions

--- Employee/urls.py
+++ Employee/urls.py
@@ -4,4 +4,8 @@ from django.urls import path
 urlpatterns = [
     path('', views.home),
     path('addemployee/', views.addemployee, name='addemployee'),
+    path('display/', views.display, name = "display"),
+    path('del/', views.delete, name='delete'),
+    path('update/', views.update, name='update'),
+
 ]
--- Employee/views.py
+++ Employee/views.py
@@ -19,3 +19,41 @@ def addemployee(request):
         print (e)
         return render(request,'index.html',{"msg": "Employee Can't be Added"})
 
+def display (request):
+    empdtls = Employee.objects.all()
+    return render (request,"index.html",{'emp':empdtls})
+
+# Employee.objects.all()               :- fetch all records
+# Employee.objects.filter(name='abc')  :-
+# Employee.objects.get(id=1)           :- Fetch only single record
+
+
+def delete(request):
+    if request.method == 'POST':
+        empname = request.POST.get('name', '')
+        empdtls = Employee.objects.filter(name=empname)
+        if empdtls.exists():
+            empdtls.delete()
+            return render(request, 'index.html', {"msg": "Deleted"})
+        else:
+            return render(request, 'index.html', {"msg": "NO RECORDS FOUND!"})
+
+    
+
+def update(request):
+    # update using filter
+    try:
+        oldname = request.POST["oldname"]
+        newname = request.POST["newname"]
+        emp = Employee.objects.filter(name=oldname)
+        if emp.exists():
+            emp.update(name=newname)
+            return render (request,"index.html",{'msg1':"Updated"})
+        else:
+            return render(request,"index.html",{'msg1':"No records found"})
+        
+    except Exception as e:
+        print(e)
+        return render(request,"index.html",{'msg1':"Not Updated"})
+
+
